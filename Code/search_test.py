abb1464927accd35f8a292d61d4a6a8c6c92105a
sprajjwal <shashwatprajjwal@gmail.com>
2020-02-04 16:49:11 -0800

    added test cases for search

--- Code/search_test.py
+++ Code/search_test.py
@@ -23,10 +23,12 @@ class SearchTest(unittest.TestCase):
         # linear search should return None for any item not in the list
         assert linear_search(names, 'Jeremy') is None
         assert linear_search(names, 'nobody') is None
+        assert linear_search(names, 'Shaash') is None
+        assert linear_search(names, 'Alan') is None
 
     def test_binary_search_with_items_in_list(self):
         # binary search requires list values to be in sorted order
-        names = ['Alex', 'Brian', 'Julia', 'Kojin', 'Nabil', 'Nick', 'Winnie']
+        names = ['Alex', 'Brian', 'Julia', 'Kojin', 'Nabil', 'Nick', 'Winnie', 'shaash']
         # binary search should return the index of each item in the list
         assert binary_search(names, 'Alex') == 0
         assert binary_search(names, 'Brian') == 1
@@ -35,6 +37,7 @@ class SearchTest(unittest.TestCase):
         assert binary_search(names, 'Nabil') == 4
         assert binary_search(names, 'Nick') == 5
         assert binary_search(names, 'Winnie') == 6
+        assert binary_search(names, 'Shaash') == 7
 
     def test_binary_search_with_items_not_in_list(self):
         # binary search requires list values to be in sorted order
@@ -42,6 +45,7 @@ class SearchTest(unittest.TestCase):
         # binary search should return None for any item not in the list
         assert binary_search(names, 'Jeremy') is None
         assert binary_search(names, 'nobody') is None
+        assert binary_search(names, 'Alan') is None
 
 
 if __name__ == '__main__':
