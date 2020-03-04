bf7e0f334c573ba8fdeffeac7cea710ce0d8d97e
sprajjwal <shashwatprajjwal@gmail.com>
2020-03-03 16:48:49 -0800

    performance updates to set

--- Code/set.py
+++ Code/set.py
@@ -1,14 +1,14 @@
 from hashtable import HashTable
 
-class mySet(object):
+class Set(object):
 
   def __init__(self, items=None):
     self.items = HashTable() # setup Hasttable
 
     # Insert everything
     if items is not None:
-            for item in items:
-                self.add(item)
+      for item in items:
+          self.add(item)
 
   @property
   def size(self):
@@ -45,30 +45,43 @@ class mySet(object):
   def union(self, other_set):
     """ Makes a union with the other set and returns a new set 
     Time complexity: O(n) since getting the keys take linear time"""
-    new_set = mySet(self.items.keys())
-    for item in other_set.items.keys():
+    smaller = self.items
+    larger = other_set.items
+    if smaller.size > larger.size:
+      smaller, larger = larger, smaller
+    new_set = Set(larger.keys())
+    for item in smaller.keys():
       new_set.add(item)
     return new_set
   
   def intersection(self, other_set):
     """ Makes an intersection between self and other set
-    Time complexity: O(n) since calling the keys() takes linear time"""
-    a = self.items
-    b = other_set.items
-    if len(a.keys()) > len(b.keys()):
-      a, b = b, a
-    new_set = mySet()
-    for x in a.keys():
-      if b.contains(x):
-        new_set.add(x)
+    Time complexity: O(min(m,n)) since we iterate over the smaller set"""
+    smaller = self.items
+    larger = other_set.items
+    if smaller.size > larger.size:
+      smaller, larger = larger, smaller
+    new_set = Set()
+    for item in smaller.keys():
+      if larger.contains(item):
+        new_set.add(item)
     return new_set
 
   def difference(self, other_set):
     """ Gets the difference between two sets and returns it.
     Time complexity: O(n) since the keys() method takes linear time"""
-    return mySet([x for x in self.items.keys() if not other_set.contains(x)])
+    return Set(x for x in self.items.keys() if not other_set.contains(x))
   
   def is_subset(self, other_set):
     """ Checks if all the items in other_set are in self
     Time complexity: O(n) since we are calling the keys() method that takes linear time"""
+    if other_set.size > self.size:
+      return False
     return len(other_set.difference(self).items.keys()) == 0
+
+if __name__ == "__main__":
+  s1 = Set([1,2, 3, 4])
+  s2 = Set([3, 4, 5, 6])
+  s3 = s1.difference(s2)
+  assert s3.items.keys() == [1, 2]
+  assert s3.length() == 2
--- Code/set_test.py
+++ Code/set_test.py
@@ -1,20 +1,20 @@
-from set import mySet
+from set import Set
 import unittest
 
 class StackTest(unittest.TestCase):
 
     def test_init(self):
-      s = mySet()
+      s = Set()
       assert s.length() == 0
       assert s.is_empty() is True
 
     def test_init_with_list(self):
-      s = mySet(['A', 'B', 'C', 'D'])
+      s = Set(['A', 'B', 'C', 'D'])
       assert s.length() == 4
       assert s.is_empty() is False
 
     def test_add(self):
-      s = mySet()
+      s = Set()
       s.add(1)
       assert s.length() == 1
       assert s.is_empty() is False
@@ -26,7 +26,7 @@ class StackTest(unittest.TestCase):
       assert s.is_empty() is False
 
     def test_length(self):
-      s = mySet()
+      s = Set()
       s.add(1)
       assert s.length() == 1
       s.add(2)
@@ -35,7 +35,7 @@ class StackTest(unittest.TestCase):
       assert s.length() == 1
 
     def test_contains(self):
-      s = mySet([1, 2, 3, 4])
+      s = Set([1, 2, 3, 4])
       assert s.length() == 4
       assert s.contains(1) == True
       assert s.contains(2) == True
@@ -46,15 +46,15 @@ class StackTest(unittest.TestCase):
       assert s.contains(2) == False
 
     def test_union(self):
-      s1 = mySet([1, 2, 3, 4])
-      s2 = mySet([3, 4, 5, 6])
+      s1 = Set([1, 2, 3, 4])
+      s2 = Set([3, 4, 5, 6])
       s3 = s1.union(s2)
       assert s3.items.keys() == [1, 2, 3, 4, 5, 6]
       assert s3.length() == 6
 
     def test_intersection(self):
-      s1 = mySet([1, 3, 4])
-      s2 = mySet([3, 4, 5, 6])
+      s1 = Set([1, 3, 4])
+      s2 = Set([3, 4, 5, 6])
       s3 = s1.intersection(s2)
       assert s3.items.keys() == [3, 4]
       assert s3.length() == 2
@@ -63,14 +63,14 @@ class StackTest(unittest.TestCase):
       assert s4.length() == 2
 
     def test_difference(self):
-      s1 = mySet([1,2, 3, 4])
-      s2 = mySet([3, 4, 5, 6])
+      s1 = Set([1,2, 3, 4])
+      s2 = Set([3, 4, 5, 6])
       s3 = s1.difference(s2)
       assert s3.items.keys() == [1, 2]
       assert s3.length() == 2
 
     def test_is_subset(self):
-      s1 = mySet([1, 2, 3, 4])
-      s2 = mySet([2, 3])
+      s1 = Set([1, 2, 3, 4])
+      s2 = Set([2, 3])
       assert s1.is_subset(s2) == True
       assert s2.is_subset(s1) == False
